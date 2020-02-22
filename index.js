1fd31a47c1fb9f2b5490bfa181700b23ea40c594
CodyPellom <paulpellom@gmail.com>
2020-02-21 22:18:19 -0500

    'okay'

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 $(function(){
-  
+//    $('.navbar-link-section').css('display','none');
     var menuButtonCounter = 0;
     $('#menu-button').click(function(){
     menuButtonCounter++;
     if (menuButtonCounter % 2 == 0){
         $('.navbar-container').css('height', '75px');
-        $('.navbar-link-section').css('display','block');
+  //      $('.navbar-link-section').css('display','block');
     } else {
         $('.navbar-container').css('height', '50px');
-        $('.navbar-link-section').css('display','none');
+    //    $('.navbar-link-section').css('display','none');
 
     }
     })
